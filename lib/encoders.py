40f1d7d60e3acd7b72c751a07382610b328e8b45
Kamran Zolfonoon <50920968+kamraz@users.noreply.github.com>
2022-10-25 20:42:12 -0400

    Revert "MR For Code Review"

--- lib/encoders.py
+++ lib/encoders.py
@@ -5,34 +5,20 @@ from .util import ndarray_from_json
 
 
 class TextEncoderPipeline:
-    # TODO move model name into config
     def __init__(self, model_name="sentence-transformers/multi-qa-mpnet-base-dot-v1", chunk_size=128):
         self.model = SentenceTransformer(model_name)
         self.chunk_size = chunk_size
     
 
-    # TODO use dataloader
     def __call__(self, text):
-        # TODO use a better tokenizer
-        tokens = self.model.tokenizer.tokenize(text) # List[str] "hello world!" -> ["hel", "##lo", "world", "##!"] -> "hello world !"
-        chunks = [
-            tokens[i:i+self.chunk_size]
-            for i in range(0, len(tokens), self.chunk_size)
-        ]
-
-        # TODO make this configurable (if we want to encode overlapping chunks)
-        # chunks.extend([
-        #     tokens[i:i+self.chunk_size]
-        #     for i in range(self.chunk_size//2, len(tokens), self.chunk_size)
-        # ])
-
+        tokens = self.model.tokenizer.tokenize(text)
+        chunks = [tokens[i:i+self.chunk_size] for i in range(0, len(tokens), self.chunk_size)]
+        chunks.extend(
+            [tokens[i:i+self.chunk_size] for i in range(self.chunk_size//2, len(tokens), self.chunk_size)]
+        )
         # convert the chunks of tokens back to text
-        text_chunks = [
-            self.model.tokenizer.convert_tokens_to_string(chunk)
-            for chunk in chunks
-        ]
-
-        embeddings = self.model.encode(text_chunks) # List[str] -> np.ndarray
+        text_chunks = [self.model.tokenizer.convert_tokens_to_string(chunk) for chunk in chunks]
+        embeddings = self.model.encode(text_chunks)
 
         # return both the embeddings and the text chunks
         return text_chunks, embeddings
