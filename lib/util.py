21cea2e9bd5035c27858a8a8a6de2f43c347b037
kamraz <50920968+kamraz@users.noreply.github.com>
2022-10-25 22:07:31 -0400

    some comments on lib files and formatting from my autoformatter lol

--- lib/util.py
+++ lib/util.py
@@ -1,14 +1,16 @@
 import base64
 import numpy as np
 
-
+# TODO Do we stil use these?
 def ndarray_to_json(ndarray):
     return {
         "shape": ndarray.shape,
         "type": str(ndarray.dtype),
-        "bytes": base64.b64encode(ndarray.tobytes()).decode("utf-8")
+        "bytes": base64.b64encode(ndarray.tobytes()).decode("utf-8"),
     }
 
 
 def ndarray_from_json(json):
-    return np.frombuffer(base64.b64decode(json["bytes"]), dtype=json["type"]).reshape(json["shape"])
+    return np.frombuffer(base64.b64decode(json["bytes"]), dtype=json["type"]).reshape(
+        json["shape"]
+    )
